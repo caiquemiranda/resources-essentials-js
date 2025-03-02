b6203eab31fcb80bf7a2bf9c668a1142a4c1a3fe
caiquemiranda <caiquemirandas@gmail.com>
2025-03-02 09:22:39 -0300

    add: basic-2.js code somar

---
+++ src/func/basico-2.js
@@ -0,0 +1,38 @@
+function somar(){
+    console.log(2+2)
+}
+
+function somar2(x){
+    console.log(x+2)
+}
+
+function somar3(x, y){
+    console.log(x+y)
+}
+
+function somar4(x, y){
+    return x + y
+}
+
+
+somar()
+somar2(2)
+somar3(3, 4)
+somar4(4, 4)
+
+
+let resultado1 = somar()
+let resultado2 = somar2(2)
+let resultado3 = somar3(3, 4)
+let resultado4 = somar4(4, 4)
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+
+console.log(resultado1 * 2)
+console.log(resultado2 - 5)
+console.log(resultado3 / 2)
+console.log(resultado4 * 10)
+

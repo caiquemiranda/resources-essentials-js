476d790d0d999b743f8c8cf4b979677fb32680d8
caiquemiranda <caiquemirandas@gmail.com>
2025-03-02 09:29:09 -0300

    add: bascio-3.js

---
+++ src/func/basic3.js
@@ -0,0 +1,13 @@
+function somar(x = 0, y = 0){
+    return x + y
+}
+
+let resultado1 = somar(2, 10)
+let resultado2 = somar(3)
+let resultado3 = somar()
+let resultado4 = somar(1, 2, 3, 4, 5, 6)
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)

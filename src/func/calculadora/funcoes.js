cf1476f9ca1d8cdbf65d68b5ff2dbff51e174c84
caiquemiranda <caiquemirandas@gmail.com>
2025-03-02 19:01:17 -0300

    add: calculadora code

---
+++ src/func/calculadora/funcoes.js
@@ -0,0 +1,17 @@
+export function somar(a, b){
+    return a + b
+}
+
+export function subtrair(a, b){
+    return a - b
+}
+
+export function dividir(a, b){
+    return a / b
+}
+
+export function multiplicar(a, b){
+    return a * b
+}
+
+//export {somar, subtrair, dividir, multiplicar}

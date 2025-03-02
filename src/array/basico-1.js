b9f9d1d5143b65801277d2843de96898e2abeb1f
caiquemiranda <caiquemirandas@gmail.com>
2025-03-02 19:38:55 -0300

    add:basico-1.js

---
+++ src/array/basico-1.js
@@ -0,0 +1,16 @@
+const numeros = [1, 2, 3, 4, 5, 6]
+
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
+console.log(numeros[5])
+console.log(numeros[6])
+
+numeros[1] = 10
+
+console.log(numeros)
+console.log(numeros.length)
+
+console.log(numeros[numeros.length -1])
